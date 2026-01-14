8396d3ac151ba48282138d985f573ace836974a3
Yu Xuan <yuxuanlow013@gmail.com>
2026-01-14 15:49:18 +0800

    Redirects

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,20 @@ const nextConfig: NextConfig = {
   devIndicators: false,
   output: "standalone",
   "allowedDevOrigins": ["http://localhost:3000"],
+  async redirects() {
+    return [
+      {
+        source: "/",
+        destination: "/ideas",
+        permanent: false,
+      },
+      {
+        source: "/:path((?!ideas|api|_next|static|public|favicon.ico).*)",
+        destination: "/ideas",
+        permanent: false,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
